ac5b65bda43fae156d61835f9e02264ea7f78ada
yblee123 <decaleooo@gmail.com>
2026-08-18 12:12:49 +0900

    BoardDTO.java

--- src/main/java/codename13/BoardDTO.java
+++ src/main/java/codename13/BoardDTO.java
@@ -2,4 +2,83 @@ package codename13;
 
 public class BoardDTO {
 
+	int boardId;
+	String category;
+	String title;
+	String content;
+	String writer;
+	String regDate;
+	int likeCount;
+	int viewCount;
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	public int getViewCount() {
+		return viewCount;
+	}
+
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardDTO [boardId=" + boardId + ", category=" + category + ", title=" + title + ", content=" + content
+				+ ", writer=" + writer + ", regDate=" + regDate + ", likeCount=" + likeCount + ", viewCount="
+				+ viewCount + "]";
+	}
 }
